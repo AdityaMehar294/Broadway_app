c9b03af747be43494a62f2ac64b635e68b074e71
Aditya Mehar <aditya.mehar@zehntech.com>
2024-02-21 11:22:27 +0530

    Add Navigation and Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_054119) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_055024) do
   create_table "plays", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
